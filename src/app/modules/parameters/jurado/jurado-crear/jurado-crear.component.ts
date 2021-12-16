664c9a73fbb49101784145ec040f86ba53631d4d
Brayan Stiven Cifuentes <1704stiven@gmail.com>
2021-12-16 04:04:28 -0500

    Importacion de jurados desde archivo plano o CSV

--- src/app/modules/parameters/jurado/jurado-crear/jurado-crear.component.ts
+++ src/app/modules/parameters/jurado/jurado-crear/jurado-crear.component.ts
@@ -58,5 +58,78 @@ export class JuradoCrearComponent implements OnInit {
     return this.dataForm.controls;
   }
 
+  public records: any[] = [];
+  uploadListener($event: any): void {
+    let text = [];
+    let files = $event.srcElement.files;
+    if (this.isValidCSVFile(files[0])) {
+      let input = $event.target;
+      let reader = new FileReader();
+      reader.readAsText(input.files[0]);
+      reader.onload = () => {
+        let csvData = reader.result;
+        console.log("hola");
+        let csvRecordsArray = (<string>csvData).split(/\r\n|\n/);
+        let headersRow = this.getHeaderArray(csvRecordsArray);
+        this.records = this.getDataRecordsArrayFromCSVFile(csvRecordsArray, headersRow.length);
+      };
+      reader.onerror = function () {
+        console.log('error is occured while reading file!');
+      };
+    } else {
+      alert("Please import valid .csv file.");
+      this.fileReset();
+    }
+  }
+  getDataRecordsArrayFromCSVFile(csvRecordsArray: any, headerLength: any) {
+    let csvArr = [];
+    for (let i = 1; i < csvRecordsArray.length; i++) {
+      let curruntRecord = (<string>csvRecordsArray[i]).split(',');
+      if (curruntRecord.length == headerLength) {
+        let csvRecord: JuradoModel = new JuradoModel();
+        csvRecord.id = parseInt(curruntRecord[0].trim());
+        csvRecord.nombre = curruntRecord[1].trim();
+        csvRecord.entidad = curruntRecord[2].trim();
+        csvRecord.telefono = curruntRecord[3].trim();
+        csvRecord.correo = curruntRecord[4].trim();
+        csvRecord.id_tipo_jurado= parseInt(curruntRecord[5].trim());
+        console.log(csvRecord.nombre);
+        csvArr.push(csvRecord);
+      }
+    }
+    return csvArr;
+  }
+  isValidCSVFile(file: any) {
+    return file.name.endsWith(".csv");
+  }
+  getHeaderArray(csvRecordsArr: any) {
+    let headers = (<string>csvRecordsArr[0]).split(',');
+    let headerArray = [];
+    for (let j = 0; j < headers.length; j++) {
+      headerArray.push(headers[j]);
+    }
+    return headerArray;
+  }
+  fileReset() {
+    this.records = [];
+  }
+  saveFromCSV(){
+    for(let i of this.records){
+      let model = new JuradoModel();
+      model.nombre = i.nombre;
+      model.correo = i.correo;
+      model.telefono = i.telefono;
+      model.entidad = i.entidad;
+      model.id_tipo_jurado = i.id_tipo_jurado;
+  
+  
+  
+      this.service.saveRecord(model).subscribe({
+        next: (data: JuradoModel) => {
+       
+        }
+      });
+    }
+  }
 
 }
